1832c45dc3e9f94e67f4b7da888ffa5c3043d47a
adkhiker <poirier.greg@gmail.com>
2019-07-21 12:08:41 -0400

    Sass styles added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
+import HomePage from "./homepage.component";
 
-function App() {
-  return (
-    <div>
-    Hello World</div>
-  );
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <HomePage />
+      </div>
+    );
+  }
 }
 
 export default App;
