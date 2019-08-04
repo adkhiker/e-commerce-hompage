52641ceef1739653655fa64ccaa80a7a31bee869
adkhiker <poirier.greg@gmail.com>
2019-08-04 19:54:21 -0400

    Shop components

--- src/index.js
+++ src/index.js
@@ -5,13 +5,9 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
-
-
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
-
-
